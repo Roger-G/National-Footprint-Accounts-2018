032505e7d7c6d9a6cd4d9021e9bd5a50a36e98bf
Jie <jie.gao1995@gmail.com>
2019-04-24 16:37:16 +0200

    Update Test1.R

--- Test1.R
+++ Test1.R
@@ -7,13 +7,16 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput("title","Title","Country VS Carbon dioxide commision"),
-      selectInput("countries","Country",choices = levels(elements$country),selected = "Japan"),
+      selectInput("countries1","Country",choices = levels(elements$country),selected = "China"),
+      selectInput("countries2", "Country",choices = levels((elements$country))),
       checkboxInput("fit", "Add line of best fit", FALSE),
       radioButtons("colour", "Point colour",
                    choices = c("blue", "red", "green", "black")),
+      radioButtons("colour1", "Point colour",
+                   choices = c("blue", "red", "green", "black")),
       numericInput("size", "Point size", 1, 1),
-      sliderInput("years","Years",min(elements$year),max(elements$year),value = c(1995,2001))
-      ),
+      sliderInput("years","Years",min(elements$year),max(elements$year),value = c(1970,2001))
+    ),
     mainPanel(
       plotlyOutput("plot")
     )
@@ -29,11 +32,13 @@ server <- function(input, output, session) {
     element2<-(element1 %>%
                  group_by(country,year,population)%>%
                  summarise(total_mean=(mean(total)),collapse=',',carbon_mean=mean(carbon),per_gdp_mean=mean(Percapita.GDP..2010.USD.)))
-    data<-subset(element2,country %in% input$countries & year>=input$years[1] & year<=input$years[2])
-    data<-as.data.frame(data)
-    p<-ggplot(data,aes(year,carbon_mean))+
-      geom_point(size = input$size, col = input$colour) +
-      scale_x_log10() +
+    data1<-subset(element2,country %in% input$countries1 & year>=input$years[1] & year<=input$years[2])
+    data2<-subset(element2,country %in% input$countries2 & year>=input$years[1] & year<=input$years[2])
+    data1<-as.data.frame(data1)
+    data2<-as.data.frame(data2)
+    p<-ggplot()+
+      geom_line(data=data1,aes(x=year,y=carbon_mean),size = input$size,col = input$colour) +
+      geom_line(data=data2,aes(x=year,y=carbon_mean),size = input$size,col = input$colour1) +
       ggtitle(input$title)
     
     if (input$fit) {
@@ -42,6 +47,6 @@ server <- function(input, output, session) {
     
     p
   })
-
+  
 }
 shinyApp(ui, server)
