6807193417f0fa9689fb43a9ff999684bd64406c
Jie <jie.gao1995@gmail.com>
2019-05-08 13:47:43 +0200

    Update Test1.R

--- Test1.R
+++ Test1.R
@@ -2,15 +2,22 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
-
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(gridExtra)
 elements<-read.csv(file.path("/Users/gaojie/R/Assignment","NFA 2018.csv"))
 element1=select(elements,country,UN_region,year,population,total,carbon,Percapita.GDP..2010.USD.)
+
 #print(element1)
+## Data preparation
+## Data preparation
 element2<-(element1 %>%
              group_by(country,year,population)%>%
              summarise(total_mean=(mean(total)),carbon=mean(carbon)))
 
-element3 = aggregate(cbind(carbon) ~ UN_region+year, element1, FUN=sum)
+element3 = aggregate(cbind(carbon,population) ~ UN_region+year, element1, FUN=sum)
+
 #print(element3)
 ui <- fluidPage(
   sidebarLayout(
@@ -50,16 +57,32 @@ ui <- fluidPage(
       sliderInput("years","Years",min(elements$year),max(elements$year),value = c(1970,2001))
     ),
     mainPanel(
-      plotlyOutput("plot")
+      plotlyOutput("plot"),
+      plotlyOutput("test"),
+      plotlyOutput("plot2")
     )
-    
   )
 )
 
 server <- function(input, output, session) {
   
-  output$plot <-renderPlotly({
+  Data <- reactive({
+    box1<-subset(element3,year>=input$years[1] & year<=input$years[2])
     
+    if (input$region=="Country and CO2 commision"){
+      data1<-subset(element2,country %in% input$countries1 & year>=input$years[1] & year<=input$years[2])
+      data2<-subset(element2,country %in% input$countries2 & year>=input$years[1] & year<=input$years[2])
+    }
+    else if (input$region=="Continent and CO2 commision"){
+      data1<-subset(element3,UN_region %in% input$continent1 & year>=input$years[1] & year<=input$years[2])
+      # 
+      # print(data1)
+      # print(data2)
+    }
+  })
+  
+  
+  output$plot<-renderPlotly({
     if (input$region=="Country and CO2 commision"){
       data1<-subset(element2,country %in% input$countries1 & year>=input$years[1] & year<=input$years[2])
       data2<-subset(element2,country %in% input$countries2 & year>=input$years[1] & year<=input$years[2])
@@ -68,15 +91,36 @@ server <- function(input, output, session) {
       data1<-subset(element3,UN_region %in% input$continent1 & year>=input$years[1] & year<=input$years[2])
       data2<-subset(element3, UN_region%in% input$continent2 & year>=input$years[1] & year<=input$years[2])
     }
-    
+    # data=curData()
+    # plot_ly(curData(),x=~year,y=~carbon,name = 'nothing',type = 'box')
     data1<-as.data.frame(data1)
     data2<-as.data.frame(data2)
-    p<-ggplot()+
+    p<-ggplot(data1,aes(x=year,y=carbon/1000000))+
       geom_line(data=data1,aes(x=year,y=carbon/1000000),size = 1,col = input$colour) +
       geom_line(data=data2,aes(x=year,y=carbon/1000000),size = 1,col = input$colour1) +
+      ylab('Total Carbon Emission / M') +
       ggtitle(input$title)
     p
   })
   
+  Data1<-reactive({
+    subset(element3,year>=input$years[1] & year<=input$years[2])
+  })
+  output$test<-renderPlotly({
+    
+    box1<-Data1()
+    p<-plot_ly(box1,x=~UN_region,y=~carbon,name = 'nothing',type = 'box')%>%
+      layout(title=sprintf("From %g to %g",input$years[1],input$years[2]))
+    p
+    # add_trace(data2x=~year,y=~carbon,mode='point')
+  })
+  output$plot2<-renderPlotly({
+    
+    # box1<-subset(element3[element3$year>=input$years[1]|element3$year<=input$years[2],])
+    box1=Data1()
+    
+    (ggplot(box1 %>% filter(UN_region != 'World'), aes(year, carbon)) + geom_area(aes(fill=UN_region), alpha=0.5) + ylab('Total Carbon Emissions') + ggtitle(sprintf("From %g to %g",input$years[1],input$years[2]))) %>%
+      ggplotly()
+  })
 }
 shinyApp(ui, server)
